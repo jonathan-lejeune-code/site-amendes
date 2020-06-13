a14d1a157d5bc308c0f05939f4cc711d36ca3ef3
jonathan-lejeune-code <lejeune.jonathan29@outlook.fr>
2020-06-13 14:23:14 +0200

    mise en forme des fichier

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const Montants = document.getElementsByClassName("Montants");
-    for (element of Montants) {
-      element.addEventListener("change", (event) => {
-        let Total = +document.getElementById("Total").innerHTML;
-        const value = +event.target.value;
-        Total = event.target.checked ? (Total + value) : (Total - value);
-        document.getElementById("Total").innerHTML = Total;
-      });
-    }
+for (element of Montants) {
+  element.addEventListener("change", (event) => {
+    let Total = +document.getElementById("Total").innerHTML;
+    const value = +event.target.value;
+    Total = event.target.checked ? (Total + value) : (Total - value);
+    document.getElementById("Total").innerHTML = Total;
+  });
+}
